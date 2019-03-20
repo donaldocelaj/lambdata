fac273b42e788fbd21c7134c61d2c8e22cb22138
Donaldo Celaj <donaldocelaj@gmail.com>
2019-03-20 06:15:56 -0400

    doing lecture activity

---
+++ lambdata_Donaldo/__init__.py
@@ -0,0 +1,9 @@
+"""
+lamdata - a colleciton fo Data Science helper functions.
+"""
+
+import pandas as import pd
+import numpy as np
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(20))
